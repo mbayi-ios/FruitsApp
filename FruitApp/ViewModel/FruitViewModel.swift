70df86292802392ca4b434e8bdf24a6924c98c35
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-10-17 13:11:51 +0300

    update on data

--- FruitApp/ViewModel/FruitViewModel.swift
+++ FruitApp/ViewModel/FruitViewModel.swift
@@ -18,9 +18,7 @@ class FruitViewModel: ObservableObject {
 
         FruitModel(name: "Amazing Avocado", price: "ksh 25/Kg", content: "Acocado contributes nearly 20 vitamins and minerals, making it a great nutrient-dense food choice. It also contains heart-healthy monounsaturated fat - good data", calories: "160", expand: ["15 g", "9 g", "2 g"], color: Color("avocadoColor"), image: "avocado"),
 
-        FruitModel(name: "Apple Fruit", price: "ksh 65/Kg", content: "Acocado contributes nearly 20 vitamins and minerals, making it a great nutrient-dense food choice. It also contains heart-healthy monounsaturated fat - good data", calories: "160", expand: ["15 g", "9 g", "2 g"], color: Color("appleColor"), image: "apple"),
-
-        FruitModel(name: "Apple Fruit", price: "ksh 60/Kg", content: "Acocado contributes nearly 20 vitamins and minerals, making it a great nutrient-dense food choice. It also contains heart-healthy monounsaturated fat - good data", calories: "160", expand: ["15 g", "9 g", "2 g"], color: Color("appleColor"), image: "apple"),
+        FruitModel(name: "Apple Fruit", price: "ksh 65/Kg", content: "Apples are considered nutrient-dense fruits, meaning they provide a lot of nutrients per serving.", calories: "160", expand: ["15 g", "9 g", "2 g"], color: Color("appleColor"), image: "apple"),
 
         FruitModel(name: "Plum Fruit", price: "ksh 45/Kg", content: "Plums may have been one of the first fruits domesticated by humans.[2] Three of the most abundantly cultivated species are not found in the wild, only around human settlements", calories: "160", expand: ["15 g", "9 g", "2 g"], color: Color("plumColor"), image: "plum"),
 
