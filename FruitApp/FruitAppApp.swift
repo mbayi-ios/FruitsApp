8bfcbccdb2f307dd0858559a7229003460d5effd
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-10-17 09:33:37 +0300

    Initial Commit

---
+++ FruitApp/FruitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitAppApp.swift
+//  FruitApp
+//
+//  Created by Amby on 17/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct FruitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
